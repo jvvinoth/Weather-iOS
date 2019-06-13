92b5e1070cde67946cc4fe64be6266673f41fcef
Vinoth <vinoth@goquo.com>
2019-06-13 11:58:28 +0800

    Project setup
    1. Created new project for basic architecture setup.

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Vinoth Varatharajan on 13/06/2019.
+//  Copyright © 2019 Vin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
